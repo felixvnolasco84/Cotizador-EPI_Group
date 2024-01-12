1c56a1dbec4389129d1a3db535c53cf7f94e05b9
Felix Vega <felix@polygonag.com>
2024-01-12 07:05:56 -0600

    a

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import QuoteForm from "./components/Quote/QuoteForm";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 w-full max-w-5xl items-center justify-center font-mono text-sm lg:flex">
         <QuoteForm />
       </div>
     </main>
