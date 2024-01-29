d497b36b7644b3cb500a02d7939248486aa4b6c9
Felix Vega <felix@polygonag.com>
2024-01-29 12:37:23 -0600

    removed manrope export

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import EPIEnergyLogo from "@/public/images/energy1.png";
 import QuoteForm from "./components/Quote/QuoteForm";
 import Image from "next/image";
 
-export const manrope = Manrope({ subsets: ["latin"] });
+const manrope = Manrope({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -12,7 +12,7 @@ export default function Home() {
       className={` ${manrope.className} flex min-h-screen flex-col items-center justify-center p-12`}
     >
       <div className="z-10 w-full max-w-5xl flex-col justify-center gap-8 font-mono text-sm lg:flex">
-        <Image src={EPIEnergyLogo} alt="EPI Energy" width={200} height={200} />        
+        <Image src={EPIEnergyLogo} alt="EPI Energy" width={200} height={200} />
         <QuoteForm />
       </div>
     </main>
